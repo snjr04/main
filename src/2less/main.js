17b3525d41e801854c0441222eaafb10d1194888
snjr04 <superkingbro004@gmail.com>
2023-02-07 13:31:12 +0300

    '2-hw'

---
+++ src/2less/main.js
@@ -0,0 +1,117 @@
+//1-задание
+let num=10
+
+console.log(num)
+
+//2-задание
+let a=10
+    b=2
+    c=5
+
+console.log(a+b+c)
+
+//3-задание
+let q=100
+
+console.log(q*2)
+
+//4-задание
+let w=67
+
+console.log(w/7)
+
+//5-задание, я не понял задание
+
+console.log(a/b)
+
+//6-задание
+
+let d=2
+
+c=15
+let result=c + d
+console.log(result)
+
+
+//7-задание
+
+a = 30
+b=15
+
+c= a+b
+
+console.log(c/4)
+
+//8-задание
+a=17
+b=10
+c=a-b
+d=20
+
+result =c+d
+
+console.log(result)
+
+//9-задание
+a='10'
+b='99'
+
+result = +a+ +b
+
+console.log(result)
+
+//10-задание
+
+let name = '18'
+let number = 10
+
+result= +name + number
+
+console.log(result)
+
+//11-задание
+
+let p =123
+console.log(typeof typeof p)
+
+//12-задание
+
+console.log(true*1)
+
+//13-задание
+
+let m="12s"
+    n="12.2"
+
+console.log(+m+ +n)
+
+
+
+
+//14-задание
+
+console.log(+null)
+
+console.log(+undefined)
+
+//14-задание по другому
+
+console.log(Boolean(undefined+true)*1)
+
+
+//15-задание
+
+console.log(+true)
+console.log(+false)
+
+//16-задание
+
+console.log('15' + 0)
+
+//17-задание
+
+let s=5
+
+s = -s
+
+console.log(s)
